ed3756cdd6cd1ca92e148677916420dda7952433
Raihanul Tomal <raihanultomal@gmail.com>
2023-04-07 16:26:40 +0600

    7th creating login&registration page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import './css/register.css';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
     <div>
-      <h1>Login page</h1>
+      <div className="container">
+        <div className="register">
+          <h3>Login page</h3>
+          <br />
+          <form>
+            <h6>Email:</h6>
+
+            <div class="form-floating mb-3">
+              <input
+                type="email"
+                class="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput">Email address</label>
+            </div>
+
+            <h6>Password:</h6>
+            <div class="form-floating my-3">
+              <input
+                type="password"
+                class="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+              />
+              <label for="floatingPassword">Password</label>
+            </div>
+
+            <Link to="/register">Dont't have an account? Register here</Link>
+            <button type="submit" class="btn btn-primary my-3 d-block">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import './css/register.css';
+import { Link } from 'react-router-dom';
 
 export default function Register() {
   return (
-    <div>
-      <h1>Register page</h1>
+    <div className="container">
+      <div className="register">
+        <h3>Register page</h3>
+        <br />
+        <form>
+          <h6>Name:</h6>
+          <div class="form-floating mb-3">
+            <input
+              type="name"
+              class="form-control"
+              id="floatingInput"
+              placeholder="Name"
+            />
+            <label for="floatingInput">Name</label>
+          </div>
+
+          <h6>Email:</h6>
+
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+
+          <h6>Password:</h6>
+          <div class="form-floating my-3">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+
+          <h6>Conform Password:</h6>
+          <div class="form-floating">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Conform Password</label>
+          </div>
+          <br />
+          <Link to="/login">Already have an account? login here</Link>
+          <button type="submit" class="btn btn-primary my-3 d-block">
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
